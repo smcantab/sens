b34aa333c3fac15bdb8ee3ffe7d41b95282bb0b1
Stefano Martiniani <stefano.martiniani@gmail.com>
2013-07-17 16:07:53 +0100

    created modify_data_file.py

--- nested_sampling/modify_data_file.py
+++ nested_sampling/modify_data_file.py
@@ -8,15 +8,21 @@ import os.path
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
+##should write it in such a way that the operations are carried out with priority determined by the order in which
+##they are listed in the command line, rather than set priority
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="load energy intervals and compute Cv stdev", 
                                      epilog="must write file name followed by a label string, otherwise raise the flag --nolabels")
     parser.add_argument("fname", nargs="+", type=str, help="filenames with heat capacity followed by label")
     parser.add_argument("--column", type=int, help="set data column to modify",default=None)
-    parser.add_argument("-m", type=float, help="factor by which multiply whole column",default=1.)
-    parser.add_argument("-d", type=float, help="factor by which divide whole column",default=1.)
-    parser.add_argument("-a", type=float, help="factor by which add whole column",default=0.)
-    parser.add_argument("-s", type=float, help="factor by which subtract whole column",default=0.)
+    parser.add_argument("-m", type=float, help="factor by which multiply whole column, priority=1",default=1.)
+    parser.add_argument("-d", type=float, help="factor by which divide whole column, priority=1",default=1.)
+    parser.add_argument("-a", type=float, help="factor by which add whole column, priority=1",default=0.)
+    parser.add_argument("-s", type=float, help="factor by which subtract whole column, priority=1",default=0.)
+    parser.add_argument("--di", type=float, help="divide this quantity by the values of the selected column, priority=2",default=None)
+    parser.add_argument("--log", action="store_true", help="take natural log of selected column, priority=3",default=False)
+    parser.add_argument("--exp", action="store_true", help="take exp of selected column, priority=4",default=False)
     args = parser.parse_args()
     
     fname = args.fname
@@ -26,7 +32,9 @@ if __name__ == "__main__":
     multiply = args.m
     divide = args.d
     subtract = args.s
-    
+    inv_divide = args.di
+    flog = args.log
+    fexp = args.exp
     
     ####################################################################################################
     #DEAL WITH INPUT
@@ -45,6 +53,12 @@ if __name__ == "__main__":
     if args.column is not None:
         for data in all_data:
             data[:][column] = (data[:][column] + add - subtract) * multiply / divide
+        if inv_divide is not None:
+            data[:][column] = inv_divide / data[:][column]
+        if flog is True:
+            data[:][column] = np.log(data[:][column])
+        if fexp is True:
+            data[:][column] = np.exp(data[:][column])
     
     for data, name in zip(all_data, all_labels):
         np.savetxt('{n}_modified.dat'.format(n=name),data)
