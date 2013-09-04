8de6b085653bb1419be93886e2ab7fc057952326
Jacob Stevenson <js850@cam.ac.uk>
2013-09-04 18:07:55 +0100

    add a test for normal nested sampling with lj

--- sens/models/_lj_tools.py
+++ sens/models/_lj_tools.py
@@ -9,19 +9,29 @@ from pele.optimize import Result
 
 from nested_sampling.utils.rotations import vector_random_uniform_hypersphere
 
+try:
+    from sens.src.runmc import mc_cython as lj_mc_cython
+except ImportError:
+    print "warning, can't import compiled mc"
+
 
 class SphericalContainerWraper(SphericalContainer):
     """return false if an atom is outside the spherical container"""
     def __call__(self, energy=None, coords=None):
         return self.accept(coords)
 
-class LJClusterNew(LJCluster):
+class LJClusterSENS(LJCluster):
     """same as LJCluster, but attach some additional information"""
     def __init__(self, natoms, bradius):
-        super(LJClusterNew, self).__init__(natoms)
+        super(LJClusterSENS, self).__init__(natoms)
         self.nzero_modes = 6
         self.k = 3 * natoms - self.nzero_modes
         self.radius = bradius # 2.5 specific to lj31, 3 specific to lj38
+        
+        self.potential = self.get_potential()
+    
+    def get_energy(self, x):
+        return self.potential.getEnergy(x)
     
 #    def get_metric_tensor(self):
 #        return None
@@ -34,13 +44,15 @@ class LJClusterNew(LJCluster):
     
     def get_random_configuration(self):
         """make sure they're all inside the radius"""
-        from pele.accept_tests import SphericalContainer
-        test = self.get_config_tests()[0]
         coords = np.zeros([self.natoms,3])
         for i in range(self.natoms):
             coords[i,:] = vector_random_uniform_hypersphere(3) * self.radius
             assert(np.linalg.norm(coords[i,:]) <= self.radius)
-        assert(test.accept(coords.flatten()))
+
+        # test to make sure the configuration is good
+        tests = self.get_config_tests()
+        for test in tests:
+            assert test.accept(coords.flatten())
         return coords.flatten()
 
     def center_coords(self, x):
@@ -48,21 +60,25 @@ class LJClusterNew(LJCluster):
         natoms = x.shape[0] 
         com = np.sum(x, 0) / natoms
         x -= com[np.newaxis, :]
-
-
-class MonteCarloCompiled(object):
-    def __init__(self, radius):
-        self.radius = radius
     
-    def __call__(self, x0, mciter, stepsize, Emax, energy, seed=None):
+    def get_mc_walker(self, mciter):
+        return LJMonteCarloCompiled(self.radius, mciter=mciter)
+
+
+class LJMonteCarloCompiled(object):
+    def __init__(self, radius, mciter=100):
+        self.radius = radius
+        self.mciter = mciter
+    
+    def __call__(self, x0, stepsize, Emax, energy, seed=None):
         if seed is None:
             seed = np.random.randint(0, sys.maxint)
-        x, energy, naccept = mc_cython(x0, mciter, stepsize, Emax, self.radius, seed)
+        x, energy, naccept = lj_mc_cython(x0, self.mciter, stepsize, Emax, self.radius, seed)
 #        print ret
         res = Result()
         res.x0 = x0
         res.x = x
-        res.nsteps = mciter
+        res.nsteps = self.mciter
         res.naccept = naccept
         res.energy = energy
         return res
