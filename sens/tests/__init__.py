2e30e8787b2056f9410aa3be46eedd42088a6815
Jacob Stevenson <js850@cam.ac.uk>
2013-09-23 14:28:23 +0100

    forgot to add the new commit to tests/__init__.py

--- sens/tests/__init__.py
+++ sens/tests/__init__.py
@@ -6,6 +6,7 @@ from _test_build_database import *
 from _test_minima_searcher import *
 from _test_sens_exact_lj import *
 from _test_sa_sampler_ns import *
+from _test_sens_exact_compute_energy_HSA import *
 
 long_test = False
 if long_test:
