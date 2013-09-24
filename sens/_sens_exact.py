0a381c7f6d33603ed6577bf8a21d99112c5dfe53
Jacob Stevenson <js850@cam.ac.uk>
2013-09-24 17:12:00 +0100

    print more status information about swaps

--- sens/_sens_exact.py
+++ sens/_sens_exact.py
@@ -18,6 +18,9 @@ class _HSASwapper(object):
         self.config_tests = config_tests
 
     def attempt_swap(self, replica, Emax):
+        res = Result()
+        res.new_replica = None
+        
         # sample a configuration from the harmonic superposition approximation
         m, xsampled = self.hsa_sampler.sample_coords(Emax)
 
@@ -25,23 +28,30 @@ class _HSASwapper(object):
         if self.config_tests is not None:
             for test in self.config_tests:
                 if not test(coords=xsampled):
-                    return None
+                    res.reason_rejected = "config_test"
+                    return res
         
         # if the energy returned by full energy function is too high, then reject the swap
         Esampled = self.potential.get_energy(xsampled)
         if Esampled >= Emax:
-            return None
+            res.reason_rejected = "Esampled too large"
+            return res
         
         # compute the energy of the replica within the superposition approximation.
         E_HSA = self.hsa_sampler.compute_energy_in_HSA(replica.energy, replica.x)
         
+        # reject if the returned energy is None.  This probably means that replica.x quenched to a minimum not in the database
+        if E_HSA is None:
+            res.reason_rejected = "E_HSA is None"
+            return res
+
         # reject if the energy is too high
-        if E_HSA is None or E_HSA >= Emax:
+        if E_HSA >= Emax:
             # no swap done
-            return None
+            res.reason_rejected = "E_HSA too large"
+            return res
 
         # return the results in a Results dictionary
-        res = Result()
         res.new_replica = Replica(xsampled, Esampled, from_random=False)
         res.minimum = m
         res.E_HSA = E_HSA
@@ -68,7 +78,51 @@ class _HSASwapperParallel(mp.Process):
             self.output_queue.put((result, r_index))
         
     
+class _SwapInfoAccumulator(object):
+    def __init__(self):
+        self._sampled_minima_counts = collections.Counter()
+        self._reasons_rejected = collections.Counter()
+    
+    def add_swap(self, old_replica, result, Emax):
+        if result.new_replica is None:
+            self._reasons_rejected[result.reason_rejected] += 1
+        else:
+            self._sampled_minima_counts[result.minimum.energy] += 1
+    
+    def number_of_rejected_swaps(self):
+        return sum(self._reasons_rejected.itervalues())
+    
+    def number_of_swaps(self):
+        return sum(self._sampled_minima_counts.itervalues())
+    
+    def number_of_swap_attempts(self):
+        return sum(self.number_of_rejected_swaps() + self.number_of_swaps())
+    
+    def minima_sampled_string(self):
+        ostr = "minima sampled: "
+        for energy, count in self._sampled_minima_counts.iteritems():
+            ostr += str(energy) + ": " + str(count) + ", "
+        if len(self._sampled_minima_counts) > 0:
+            # remove the comma and space before returning
+            ostr = ostr[:-2]
+        return ostr
+    
+    def reasons_rejected_string(self):
+        ostr = "swaps rejected: "
+        for reason, count in self._reasons_rejected.iteritems():
+            ostr += str(reason) + ": " + str(count) + ", "
+        if len(self._reasons_rejected) > 0:
+            # remove the comma and space before returning
+            ostr = ostr[:-2]
+        return ostr
+    
+    def print_info(self):
+        print self.minima_sampled_string()
+        print self.reasons_rejected_string()
          
+        
+        
+        
 
 
 class NestedSamplingSAExact(NestedSampling):
@@ -135,12 +189,10 @@ class NestedSamplingSAExact(NestedSampling):
         
         self._set_up_parallelization_swap()
         
-        self._sampled_minima_counts = collections.Counter()
-    
+        self._swap_info = _SwapInfoAccumulator()
 
-    @property
     def number_swaps_accepted(self):
-        return sum(self._sampled_minima_counts.values())
+        return self._swap_info.number_of_swaps()
 
     def _set_up_parallelization_swap(self):
         """set up the parallel workers for doing swap attempts"""
@@ -166,13 +218,14 @@ class NestedSamplingSAExact(NestedSampling):
                 worker.join()
 
     
-    def _print_swap_info(self, old_replica, result, Emax):
-        if self.verbose and result is not None:
-            print "accepting swap %d: Eold %g Enew %g Eold_SA %g Emax %g m.energy %g" % (
-                        self.iter_number, old_replica.energy, result.new_replica.energy, 
-                        result.E_HSA, Emax, result.minimum.energy)
-        self._sampled_minima_counts[result.minimum.energy] += 1
-        
+    def _accumulate_swap_info(self, old_replica, result, Emax):
+        self._swap_info.add_swap(old_replica, result, Emax)
+#        if result.new_replica is not None:
+#            if self.verbose:
+#                print "accepting swap %d: Eold %g Enew %g Eold_SA %g Emax %g m.energy %g" % (
+#                            self.iter_number, old_replica.energy, result.new_replica.energy, 
+#                            result.E_HSA, Emax, result.minimum.energy)
+
 
     def _attempt_swaps_parallel(self, replicas, Emax):
         """do all the swap attempts in parallel"""
@@ -186,9 +239,9 @@ class NestedSamplingSAExact(NestedSampling):
         # retrieve the results from the queue
         for i in xrange(len(replicas)):
             res, index = self._swap_get_queue.get()
-            if res is not None:
+            self._accumulate_swap_info(replicas[index], res, Emax)
+            if res.new_replica is not None:
 #                self.count_sampled_minima += 1
-                self._print_swap_info(replicas[index], res, Emax)
                 replicas[index] = res.new_replica
         
         assert self._swap_get_queue.empty() 
@@ -199,9 +252,9 @@ class NestedSamplingSAExact(NestedSampling):
         for i in xrange(len(replicas)):
             r = replicas[i]
             res = self.hsa_swapper.attempt_swap(r, Emax)
-            if res is not None:
+            self._accumulate_swap_info(replicas[i], res, Emax)
+            if res.new_replica is not None:
 #                self.count_sampled_minima += 1
-                self._print_swap_info(replicas[i], res, Emax)
                 replicas[i] = res.new_replica
 
     def _attempt_swaps(self, replicas, Emax):
@@ -227,7 +280,7 @@ class NestedSamplingSAExact(NestedSampling):
         self._times.sampling += t1 - t0
         if self.verbose and self.iprint > 0 and self.iter_number % self.iprint == 0:
             print "time in mc walk", self._times.mc, "sampling", self._times.sampling, "tot", t2 - self._times.at_start, "this iter", t2-t1, t1-t0
-            print self._sampled_minima_counts 
+            self._swap_info.print_info() 
         return replicas
         
         
--- sens/tests/_test_sens_exact_lj.py
+++ sens/tests/_test_sens_exact_lj.py
@@ -68,7 +68,7 @@ class TestSENSExact_LJ(_test_ns_lj.TestNS_LJ):
     
     def test1(self):
         super(TestSENSExact_LJ, self).test1()
-        self.assertGreater(self.ns.number_swaps_accepted, 0)
+        self.assertGreater(self.ns.number_swaps_accepted(), 0)
         
         
         
